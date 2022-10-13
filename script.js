0d5b982258bd0d198198a16b58f963cbd5b810e6
Usman077 <usmansheikh4949@gmail.com>
2022-10-13 22:20:30 +0500

    random genrator

---
+++ script.js
@@ -0,0 +1,45 @@
+// creating characters
+const upperset ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerset ="abcdefghijklmnopqrstuvwxyz";
+const numberset ="0123456789";
+const symbolset ="~!@#$%^&*+-/"
+// storing the inputs in variables
+const box = document.getElementById("password")
+const pass = document.getElementById("pass")
+const uppercase = document.getElementById("uppercase")
+const lowercase = document.getElementById("lowercase")
+const number = document.getElementById("number")
+const symbol = document.getElementById("symbol")
+
+
+
+//creating formula for rendom password
+const formula = (dataSet)=> {
+  return dataSet[ Math.floor(Math.random() * dataSet.length)]
+};
+// creating the fuction  to check the inputs
+const generatepass = (password= "")=>{
+     if (uppercase.checked){
+    password += formula(upperset)
+}
+     if (lowercase.checked){
+    password += formula(lowerset)
+}
+     if (number.checked){
+    password += formula(numberset)
+}
+     if (symbol.checked){
+    password += formula(symbolset)
+}
+   if (password.length<pass.value){
+    generatepass();
+   }
+ box.innerHTML= password;   
+  
+}
+
+document.getElementById("btn").addEventListener("click",
+()=>{
+    generatepass();
+})
+console.log(pass.value)
